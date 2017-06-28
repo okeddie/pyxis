9ebc6e5cdab571be4fb7ba939e76389cc8087f89
Eddie Vasquez <eddie.p.vasquez@gmail.com>
2017-06-28 09:16:07 -0500

    making a dumb test script.

---
+++ init.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+'''
+This is a test init.py
+'''
+
+import sys
+import os
+
+list1 = ['1', '2', '3']
+list2 = ['red', 'orange', 'green', 'blue']
+mash_lists = zip(list1, list2)
+
+print(mash_lists)
